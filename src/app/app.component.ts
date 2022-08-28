9e61cee8925d6d03483cb09596ff4bbec6c0646b
acsnevesgit <acsneves@outlook.com>
2022-08-28 16:10:45 +0200

    Add very simple form input validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,35 @@
 import { Component } from '@angular/core'
+import { FormControl, Validators } from '@angular/forms'
 import { interval, Observable } from 'rxjs'
 import { map, shareReplay } from 'rxjs/operators'
 
 import { timeI } from './core/timeI'
 import { calcDateDiff } from './timediff.service'
 
-// Defines a class that contains application data and logic, and is associated with an HTML template that defines a view to be displayed in a target environment
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'natural-cycles-FE-challenge'
-  public eventName: string = 'Midsummer Eve'
-  public eventDate: string = '2023-06-24'
+  // ------------------------------ Variable declarations ------------------------------
+  title: string = 'natural-cycles-FE-challenge'
+  eventNameControl: FormControl
+  eventDateControl: FormControl
+  eventName: string
+  eventDate: string
+  public timeRemaining$: Observable<timeI>
 
   constructor() {
+    // ------------------------------ Variable initializations ------------------------------
+    this.eventName = 'Midsummer Eve'
+    this.eventDate = '2023-06-24'
+    this.eventNameControl = new FormControl(
+      'Midsummer Eve',
+      Validators.required,
+    )
+    this.eventDateControl = new FormControl('2023-06-24', Validators.required)
+
     // uses pipe to change the observable
     this.timeRemaining$ = interval(1000).pipe(
       map((x) => calcDateDiff()),
@@ -24,16 +37,18 @@ export class AppComponent {
     )
   }
 
-  // Using an Angular event: will trigger with each input change
-  onEventChangeFn(value: string) {
+  // ------------------------------ Methods--------------------------------------------------
+  onEventNameChangeFn(value: string) {
     this.eventName = value
-    console.log('event changed ' + value)
+    console.log('event name has changed: ' + value)
   }
 
-  // Alternatively, using a DOM event: will only fire when the user has blurred the input
-  // changeFn(event: { target: { value: string } }) {
-  //   this.eventName = event.target.value
-  // }
+  onEventDateChangeFn(value: string) {
+    this.eventDate = value
+    console.log('event date has changed: ' + value)
+  }
 
-  public timeRemaining$: Observable<timeI>
+  onFormSubmit(formData: any) {
+    console.log(formData)
+  }
 }
--- src/app/timediff.service.ts
+++ src/app/timediff.service.ts
@@ -4,19 +4,20 @@ import { timeI } from './core/timeI'
 // eventDate = Jun 24 2023
 
 // TODO: change strictly typed date
-export function calcDateDiff(eventDate: Date = new Date('2023-06-24')): timeI {
+// export class timediffService {
+export function calcDateDiff(eventTimer: Date = new Date('2023-06-24')): timeI {
   // JavaScript will (by default) output dates in full text string format - e.g. Wed Mar 25 2015 01:00:00 GMT+0100 (Central European Standard Time)
+  // console.log(eventDate)
 
-  // ----- Time values -----
+  // ------------------------------ Time values ------------------------------
   const hoursInDay = 24
   const minutesInHour = 60
   const secondsInMinute = 60
   const millisecondsInSecond = 1000
 
-  const eventDay = eventDate.valueOf() // ms
-  const timeDiff = eventDay - Date.now()
+  const timeDiff = eventTimer.valueOf() - Date.now() // gives the difference in ms
 
-  // ----- Time conversions -----
+  // ------------------------------ Time conversions ------------------------------
   // Math.floor() returns actually elapsed seconds
   const daysToEvent = Math.floor(
     timeDiff /
@@ -33,11 +34,10 @@ export function calcDateDiff(eventDate: Date = new Date('2023-06-24')): timeI {
     (timeDiff / millisecondsInSecond) % secondsInMinute,
   )
 
-  // console.log(eventDate)
-
   // TODO: remove console log
   // console.log(
   //   `The time left to the event is ${daysToEvent} days, ${hoursToEvent} hours, ${minutesToEvent} minutes and ${secondsToEvent} seconds`,
   // )
+
   return { daysToEvent, hoursToEvent, minutesToEvent, secondsToEvent }
 }
